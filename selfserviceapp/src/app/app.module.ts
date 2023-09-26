0b8ababacf978737dc77a66d9d4d07fad1abc98a
Iswarya G <iswarya.g@prodapt.com>
2023-09-26 14:39:56 +0530

    project added

---
+++ selfserviceapp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogComponent } from './login/log/log.component';
+import { RegComponent } from './register/reg/reg.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogComponent,
+    RegComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
